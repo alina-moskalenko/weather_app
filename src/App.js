ce03aced8177e0745a336e4ad24ede748c37f4ed
alina-moskalenko <alinaamoskalenko@gmail.com>
2024-03-09 00:12:42 -0800

    trying to find solutions to upload current data from demo.php url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
 import React from "react";
-import jsonData from "./dpk_weather_data.json";
+import jsonData from "../src/dpk_weather_data.json";
 
 //See what the current data is
+// const getJSON = function (url, callback) {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("get", url, true);
+//   xhr.responseType = "json";
+//   xhr.onload = function () {
+//     let status = xhr.status;
+//     if (status == 200) {
+//       callback(null, xhr.response);
+//     } else {
+//       callback(status);
+//     }
+//   };
+//   xhr.send();
+// };
+
+// getJSON("https://mm214.com/demo.php", function (err, data) {
+//   if (err != null) {
+//     alert("Something went wrong: " + err);
+//   } else {
+//     //console.log(data);
+//   }
+// });
+
+// Trying fetch api
+// fetch("https://mm214.com/demo.php")
+//   .then((response) => response.text())
+//   .then((html) => console.log(html));
+
+// Trying async/await fetch
+// async function postData(url = "", data = {}) {
+//   // Default options are marked with *
+//   const response = await fetch(url, {
+//     method: "POST", // *GET, POST, PUT, DELETE, etc.
+//     mode: "cors", // no-cors, *cors, same-origin
+//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: "same-origin", // include, *same-origin, omit
+//     headers: {
+//       "Content-Type": "application/json",
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: "follow", // manual, *follow, error
+//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     body: JSON.stringify(data), // body data type must match "Content-Type" header
+//   });
+//   return response.json(); // parses JSON response into native JavaScript objects
+// }
+
+// postData("https://mm214.com/demo.php", { answer: 42 }).then((data) => {
+//   console.log(data); // JSON data parsed by `data.json()` call
+//   let jsonData = data;
+// });
 
 // Convert Kelvin to Fahrenheit for temps
 let mainTemp = Math.floor((jsonData.main.temp - 273.15) * 1.8 + 32);
